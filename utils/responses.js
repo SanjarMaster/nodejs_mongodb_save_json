1c43097447417a0b848d2f85fecdd4acd6da04f2
SanjarMaster <84626477+SanjarMaster@users.noreply.github.com>
2022-11-26 20:49:56 +0500

    Create responses.js

---
+++ utils/responses.js
@@ -0,0 +1,13 @@
+exports.status = (status, values, result,res) => {
+
+    const data = {
+        "res_id": status,
+        "mess": values,
+        "result": result
+    }
+
+    res.status(200)
+    res.json(data)
+    res.end()
+
+}
